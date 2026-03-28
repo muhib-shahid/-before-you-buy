949b6dd5e998763e80d29246a67f64f67e1574cc
Muhib Shahid <muhibshahid@gmail.com>
2026-03-28 20:17:40 +0500

    Improve UI: enhanced buttons, forms, and admin tables; fixed authentication sync

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'typeface-merriweather';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { syncSession } from './utils/syncSession';
 
